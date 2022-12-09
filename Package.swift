f15e1384dc5a45cd3dd6a6735c45c18f9b1313b4
Jimmy Morales <me@jimmymorales.dev>
2022-12-08 22:02:00 -0600

    Prepare for release 0.2.0.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots//dev/jimmymorales/core-kmmbridge/0.1.0-SNAPSHOT/core-kmmbridge-0.1.0-SNAPSHOT.zip"
+let remoteKotlinUrl = "https://s01.oss.sonatype.org/service/local/staging/deployByRepositoryId/devjimmymorales-1009//dev/jimmymorales/core-kmmbridge/0.2.0/core-kmmbridge-0.2.0.zip"
 let remoteKotlinChecksum = "7020c9684f5a463a9b0771f864a75aae65f3110f99bcde735f55ffe987314e19"
 let packageName = "FeatureFlagsCore"
 
