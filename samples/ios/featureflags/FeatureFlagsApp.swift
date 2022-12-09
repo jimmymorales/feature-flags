dd0b7e75e29593d4ef5ac8667e7b7fc757f5c9a7
Jimmy Morales <me@jimmymorales.dev>
2022-12-08 22:38:23 -0600

    chore: create ios app from XCode template

---
+++ samples/ios/featureflags/FeatureFlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  featureflagsApp.swift
+//  featureflags
+//
+//  Created by Jimmy Morales on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FeatureFlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
